a077b58169ec492f0edc7c03e4b1b188a4132d8a
Prateek Gianchandani <prateek.searchingeye@gmail.com>
2014-02-10 18:01:59 +0530

    Fixed issue with parse

---
+++ DVIA/DamnVulnerableIOSApp/DamnVulnerableIOSApp/FBMissingSymbols.m
@@ -0,0 +1,31 @@
+//
+//  FBMissingSymbols.m
+//  DamnVulnerableIOSApp
+//
+//  Created by Prateek Gianchandani on 2/10/14.
+//  Copyright (c) 2014 HighAltitudeHacks.com. All rights reserved.
+//
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
