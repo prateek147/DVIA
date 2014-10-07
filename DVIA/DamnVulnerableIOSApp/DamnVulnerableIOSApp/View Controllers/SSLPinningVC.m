f99f2f46b4db23a7ca307a21cb9218cc37b78d4e
Prateek Gianchandani <prateekg147@Prateeks-MacBook-Pro.local>
2014-10-07 23:36:29 +0400

    UI Implemented for SSL Pinning

---
+++ DVIA/DamnVulnerableIOSApp/DamnVulnerableIOSApp/View Controllers/SSLPinningVC.m
@@ -0,0 +1,38 @@
+//
+//  SSLPinningVC.m
+//  DamnVulnerableIOSApp
+//
+//  Created by Prateek Gianchandani on 10/7/14.
+//  Copyright (c) 2014 HighAltitudeHacks.com. All rights reserved.
+//
+
+#import "SSLPinningVC.h"
+#import "UIViewController+ECSlidingViewController.h"
+
+@interface SSLPinningVC ()
+
+@end
+
+@implementation SSLPinningVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
