7c3da144008171f10a461dd848869a91f1141a6f
Egor Tolstoy <igrekde@gmail.com>
2015-02-08 21:16:37 +0300

    Added new TableViewController with a list of different third party services

---
+++ DVIA/DamnVulnerableIOSApp/DamnVulnerableIOSApp/View Controllers/AttackingThirdPartyLibrariesTableViewController.m
@@ -0,0 +1,24 @@
+//
+//  AttackingThirdPartyLibrariesTableViewController.m
+//  DamnVulnerableIOSApp
+//
+//  Created by Egor Tolstoy on 08.02.15.
+//  Copyright (c) 2015 HighAltitudeHacks.com. All rights reserved.
+//
+
+#import "AttackingThirdPartyLibrariesTableViewController.h"
+
+@interface AttackingThirdPartyLibrariesTableViewController ()
+
+@end
+
+@implementation AttackingThirdPartyLibrariesTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationController.navigationBar.tintColor = kNavigationTintColor;
+}
+
+
+@end
