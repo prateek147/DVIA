18f57d2610124bb732e574056145e27806e39f3e
Egor Tolstoy <igrekde@gmail.com>
2015-05-18 22:59:21 +0300

    Added Apple Watch target

---
+++ DVIA/DamnVulnerableIOSApp/DamnVulnerableIOSApp WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  DamnVulnerableIOSApp WatchKit Extension
+//
+//  Created by Egor Tolstoy on 18/05/15.
+//  Copyright (c) 2015 HighAltitudeHacks.com. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
